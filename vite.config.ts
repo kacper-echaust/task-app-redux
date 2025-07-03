d5ba02665a53e9e9f14fa30f8dda9663237d7642
kacper-echaust <k9echaust@gmail.com>
2025-07-03 16:54:13 +0200

    gh deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	base: '/task-app-redux',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
